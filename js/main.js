2be6cc8651679913e374d255cde4b447eccab254
Dawid <dawidwojcik476@gmail.com>
2021-04-03 10:35:50 +0200

    Added burger menu JS script

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 const menuBtn = document.querySelector('.menu-link');
 const mainMenu = document.querySelector('.menu');
-const navBar = document.querySelector('.navbar');
-const logoImg = document.querySelector('.logo');
-const contact = document.querySelector('.contact');
-const headerBgc = document.querySelector('.headerbgc');
+
+
+
 let menuOpen = false;
 
 
@@ -16,18 +15,6 @@ const menu = () => {
         menuBtn.classList.remove('open');
         mainMenu.classList.remove('open');
         menuOpen = false;
-    }
-}
-
-const stickymenu = () => {
-    if (window.scrollY > (500)) {
-        navBar.classList.add("sticky");
-        logoImg.classList.add("sticky");
-        contact.classList.add("sticky");
-    } else {
-        navBar.classList.remove("sticky");
-        logoImg.classList.remove("sticky");
-        contact.classList.remove("sticky");
     }
 }
 
